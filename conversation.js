a96d41d6419c4ef333b7407cd80e95abb3ff244e
chad1008 <13856531+chad1008@users.noreply.github.com>
2021-11-24 07:31:16 -0500

    update findConversation() to scale for larger orgs

--- conversation.js
+++ conversation.js
@@ -9,39 +9,77 @@ const app = new App( {
 // Retrieve the unique ID of the requested contersation
 async function findConversation( conversationName ) {
 	let conversationId = null;
-	// Look for a user with name requested
-	try {
-		const userList = await app.client.users.list( {
-			token: process.env.SLACK_USER_TOKEN,
-		} );
-		for ( const user of userList.members ) {
-			if ( user.name === conversationName ) {
-				conversationId = user.id;
-
-				return conversationId;
+	const limit = 500;
+	// Look for a channel with the name requested, limited to channels the user is already a member of
+	let channelCursor = '';
+	do {
+		try {
+			const channelList = await app.client.users.conversations( {
+				token: process.env.SLACK_USER_TOKEN,
+				types: 'public_channel, private_channel',
+				cursor: channelCursor,
+				limit: limit,
+				exclude_archived: true,
+			} );
+			for ( const channel of channelList.channels ) {
+				if ( channel.name === conversationName ) {
+					conversationId = channel.id;
+					return conversationId;
+				}
 			}
+			channelCursor =
+				channelList.response_metadata.next_cursor !== ''
+					? channelList.response_metadata.next_cursor
+					: '';
+		} catch ( error ) {
+			console.error( error );
 		}
-	} catch ( error ) {
-		console.error( error );
-	}
-	// Since we haven't returned a user, look for a matching channel
-	try {
-		const channelList = await app.client.conversations.list( {
-			token: process.env.SLACK_USER_TOKEN,
-			types: 'public_channel, private_channel',
-		} );
-		for ( const channel of channelList.channels ) {
-			if ( channel.name === conversationName ) {
-				conversationId = channel.id;
+	} while ( channelCursor !== '' );
+	// Since we didn't return a matching channel, search for matching usernames
+	let userCursor = '';
+	let userBatchCounter = 1;
+	do {
+		// TODO: Tie the following in-progress messages to a config setting
+		// Initial in-progress message on the fourth batch
+		if ( userBatchCounter === 4 ) {
+			console.log(
+				`[${
+					userBatchCounter / 4
+				}] Searching for users named ${ conversationName }. This might take a while on larger teams...`
+			);
+		}
+		// Follow-up in-progress message every fourth batch
+		if ( userBatchCounter > 4 && userBatchCounter % 4 === 0 ) {
+			console.log(
+				`[${
+					userBatchCounter / 4
+				}] Still looking for ${ conversationName }. There are a lot of users here...`
+			);
+		}
 
-				return conversationId;
+		try {
+			const userList = await app.client.users.list( {
+				token: process.env.SLACK_USER_TOKEN,
+				cursor: userCursor,
+				limit: limit,
+			} );
+			for ( const user of userList.members ) {
+				if ( user.name === conversationName ) {
+					conversationId = user.id;
+					return conversationId;
+				}
 			}
+			userCursor =
+				userList.response_metadata.next_cursor !== ''
+					? userList.response_metadata.next_cursor
+					: '';
+			userBatchCounter++;
+		} catch ( error ) {
+			console.error( error );
 		}
-	} catch ( error ) {
-		console.error( error );
-	}
-	// If conversationID is still null, output an error
+	} while ( userCursor !== '' );
 	if ( conversationId === null ) {
+		// If conversationID is still null, output an error
 		console.log(
 			`Sorry, I can't find a user or channel named ${ conversationName }`
 				.red
